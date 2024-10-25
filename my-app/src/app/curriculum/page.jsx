f1401489ab5cf6d081e09af570f71ea55ffec53d
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-10-25 02:10:20 -0500

    minor change

--- my-app/src/app/curriculum/page.jsx
+++ my-app/src/app/curriculum/page.jsx
@@ -45,7 +45,7 @@ const Curriculum = () => {
               marginBottom: '20px',
             }}
           >
-            Curriculum
+            Curr
           </Typography>
           <Typography
             variant="body1" // Change variant to body1 for paragraph
